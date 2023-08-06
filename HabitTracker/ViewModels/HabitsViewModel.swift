f15ff895be06530429a4037de18d3903bdc08351
Ty <tylertorresp@gmail.com>
2023-08-05 22:20:37 -0700

    View model update.

--- HabitTracker/ViewModels/HabitsViewModel.swift
+++ HabitTracker/ViewModels/HabitsViewModel.swift
@@ -12,7 +12,6 @@ import UIKit
 class HabitsViewModel : ObservableObject {
     
     // View Models let you separate out logic from the UI layer
-    
     @Published var habits : [Habit] = [
         Habit(id: 1, name: "Read 10 Pages", icon: Iconoir.refresh.rawValue),
         Habit(id: 1, name: "Walk a Mile", icon: Iconoir.refresh.rawValue),
@@ -44,7 +43,7 @@ class HabitsViewModel : ObservableObject {
     func saveNewHabit2() {
         guard let device_id = UIDevice.current.identifierForVendor else { return }
         
-        var newHabit = Habit(id: habits.count + 1, name: habitNameInput, icon: selectedIcon.rawValue)
+        let newHabit = Habit(id: habits.count + 1, name: habitNameInput, icon: selectedIcon.rawValue)
         habits.append(newHabit)
         clearInput()
         
